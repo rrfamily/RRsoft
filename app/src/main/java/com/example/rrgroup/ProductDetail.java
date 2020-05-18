903b4f807f0c740e296b0f951f63c4ec7064dc74
rrfamily <rrfamilyofficial1@gmail.com>
2020-05-19 00:30:58 +0300

    added prodcut detail.xml

---
+++ app/src/main/java/com/example/rrgroup/ProductDetail.java
@@ -0,0 +1,16 @@
+package com.example.rrgroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+
+
+    }
+}
